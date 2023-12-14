efc2a62521dec4cc21fa481622baa92c9d825484
Ranjit Kumar <ranjit.techexactly@gmail.com>
2023-12-14 15:07:13 +0530

    fixed

--- Example/CustomSlider/ViewController.swift
+++ Example/CustomSlider/ViewController.swift
@@ -16,7 +16,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         self.customSlider.configureSlider(trackHeight: 10, thumbRadius: 30, borderWidth: 5, thumbColor: .green, thumbBorderColor: .black, trackColor: .red)
         
-        
     }
 
     override func didReceiveMemoryWarning() {
